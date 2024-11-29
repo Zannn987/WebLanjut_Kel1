e6cec981efb2615cffad6921cd434cebf7f1593e
Salsahumayhira <167851470+Salsahumayhira@users.noreply.github.com>
2024-11-29 15:54:38 +0700

    membuat_home

---
+++ admin/home.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+   
+    
+    <h1 class="display-4">Welcome to TI Department</h1>
+    <p class="lead">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa maxime commodi earum eaque nostrum pariatur, dolore at. 
+        Molestiae nulla ullam fugiat aliquam, ut recusandae cum fugiat magnam veritatis perferendis cupiditate 
+        accusantium doloribus saepe, rem non laudantium iure earum quo corrupti unde harum! Quibusdam inventore ut 
+        reprehenderit nulla deleniti! Minus dolorum autem deleniti natus tenetur quia? Consequuntur ex doloribus 
+        possimus saepe praesentium? Minus officia qui aliquid quasi nobis reiciendis, provident sit voluptatem consectetur 
+        optio! Totam ipsa amet optio tempore, architecto consequatur tempora aperiam veniam aspernatur accusantium nemo sunt.
+        Atque non nemo assumenda, praesentium eligendi rem, unde laboriosam explicabo neque beatae voluptate!
+    </p>
+</div>
